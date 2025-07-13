c12a453eeb6bcadb608f81b9d31620ba814232b9
Christopher <158623664+Program05@users.noreply.github.com>
2025-07-13 14:12:34 -0400

    Add Parasyte to Must Watch table and update nav styles

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Simple JS form validation
 document.querySelector(".search-form").addEventListener("submit", function (e) {
   const search = document.querySelector('input[name="q"]').value.trim();
   if (!search) {
